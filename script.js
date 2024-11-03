d6b1d6b8ac3c3a6c7c82d7a58937348ffcfbe94f
luan-alvesdev <lluan.alves1@gmail.com>
2024-11-03 19:52:11 -0300

    :construction: ajustou menu

--- script.js
+++ script.js
@@ -1,51 +1,3 @@
-//Loader
-document.addEventListener('DOMContentLoaded', () =>{
-    const tl = gsap.timeline()
-
-    // tl.fromTo('.loader', 1,
-    // { width: '100%'},
-    // {width: '0%', 
-    // delay:3.5,
-    // ease: Expo.easeInOut})
-
-    .fromTo('.navbar', 1,
-    {y: 50, opacity:0},
-    {y: 0, opacity: 1,ease: Expo.ease}, '-=0.5')
-
-    .fromTo('.info-title', 1.5,
-    {x: -50, opacity:0},
-    {x: 0, opacity: 1,ease: Expo.ease}, '-=0.5')
-
-    .fromTo('.about-logo', 1.5,
-    {x: 50, opacity:0},
-    {x: 0, opacity: 1,ease: Expo.ease}, '-=1.5')
-
-    .fromTo('.info-p', 1.5,
-    {x: 100, opacity:0},
-    {x: 0, opacity: 1,ease: Expo.ease}, '-=0.5')
-
-    .fromTo('.project-text', 1.5,
-    {x: 50, opacity:0},
-    {x: 0, opacity: 1,ease: Expo.ease}, '-=01')
-
-    .fromTo('.slidebar', 1.5,
-    {display:"none"},
-    {display:"flex", ease: Expo.ease}, '-=1.5')
-
-    .fromTo('.projects', 1.5,
-    {display:"none"},
-    {display:"flex", ease: Expo.ease}, '-=1.5')
-
-    .fromTo('.contact', 1.5,
-    {display:"none"},
-    {display:"block", ease: Expo.ease}, '-=1.5')
-
-    .fromTo('footer', 1.5,
-    {display:"none"},
-    {display:"block", ease: Expo.ease}, '-=1.5')
-    
-})
-
 //Scroll
 const btnScroll = document.querySelector('.btn-scroll')
 
@@ -56,18 +8,8 @@ btnScroll.addEventListener('click', ()=>{
     })
 })
 
-//Mobile Menu
-const mobileMenu = document.querySelector(".js-mobile-menu")
-const btnMobile = document.querySelector(".js-btn-sidebar")
-let cv = "https://docs.google.com/document/d/17Zhb6VYRw3Hv9nAmQYZMt3Y3iF18BWKq1xKuigYgBzE/export?format=pdf"
 // URL do Google Docs modificada para baixar como PDF
-
-
-btnMobile.addEventListener("click", (event) =>{
-    event.preventDefault()
-
-    mobileMenu.classList.toggle("show")
-})
+let cv = "https://docs.google.com/document/d/17Zhb6VYRw3Hv9nAmQYZMt3Y3iF18BWKq1xKuigYgBzE/export?format=pdf"
 
 function mudarIdioma(idioma) {
     initInternacionalizacao(idioma);
@@ -102,4 +44,3 @@ function downloadFile() {
         })
         .catch(error => console.error('Erro ao baixar o arquivo:', error));
 }
-
